ef34f576432d9cea6a1102aa79597fdf4f466997
Una Kravets <una.kravets@gmail.com>
2019-02-24 23:29:14 -0500

    GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        //trackingId: "ADD YOUR TRACKING ID HERE",
+        trackingId: "UA-36758177-13",
       },
     },
     "gatsby-plugin-offline",
