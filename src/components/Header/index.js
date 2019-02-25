e57980f66c5047a09f55087f02e2522967e1c161
Una Kravets <una.kravets@gmail.com>
2019-02-25 00:06:18 -0500

    optimizing codes and base shakshuka recipe :eggs:

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,81 +3,41 @@ import Link from 'gatsby-link'
 import style from './style.module.css'
 
 export default class Header extends React.Component {
+
   render() {
+    const navItems = ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Snacks']
+
     return (
       <header className={style.header}>
         <Link to={'/'} className={style.logo}>
           <h1>keto.nyc</h1>
         </Link>
         <ul className={style.nav}>
-          <li className={style.navItem}><Link className={style.navLinkMore} to={'/'}>Snacks</Link>
+        {navItems.map(item => {
+          return(
+            <li className={style.navItem}><Link className={style.navLinkMore} to={'/'}>{item}</Link>
             <ul className={style.moreContent}>
               {
                 this.props.posts.map(({ node }) => {
                   const post = node.frontmatter
                   if (
-                    post.published && post.tags.includes('snack')
+                    post.published && post.tags.includes(item.toLowerCase())
                   ) {
-                    return (
-                      <li>
-                        <Link to={node.fields.slug}>
-                          <figure className={style.mediaContainer}>
-                            <img src={post.image1.childImageSharp.resolutions.src} srcSet={post.image1.childImageSharp.resolutions.srcSet} />
-                          </figure> 
-                          <h3>{post.title}</h3>
-                        </Link>
-                      </li>
-                    )
-                  }
+                  return (
+                    <li>
+                      <Link to={node.fields.slug}>
+                        <figure className={style.mediaContainer}>
+                          <img src={post.image1.childImageSharp.resolutions.src} srcSet={post.image1.childImageSharp.resolutions.srcSet} />
+                        </figure> 
+                        <h3>{post.title}</h3>
+                      </Link>
+                    </li>
+                  )}
                 })}
             </ul>
           </li>
-          <li className={style.navItem}>
-          <Link className={style.navLinkMore} to={'/'}>Meals</Link>
-            <ul className={style.moreContent}>
-              {
-                this.props.posts.map(({ node }) => {
-                  const post = node.frontmatter
-                  if (
-                    post.published && post.tags.includes('meal')
-                  ) {
-                    return (
-                      <li>
-                        <Link to={node.fields.slug}>
-                          <figure className={style.mediaContainer}>
-                            <img src={post.image1.childImageSharp.resolutions.src} srcSet={post.image1.childImageSharp.resolutions.srcSet} />
-                          </figure>
-                          <h3>{post.title}</h3>
-                        </Link>
-                      </li>
-                    )
-                  }
-                })}
-            </ul>
-          </li>
-          <li className={style.navItem}><Link className={style.navLinkMore} to={'/'}>Desserts</Link>
-            <ul className={style.moreContent}>
-              {
-                this.props.posts.map(({ node }) => {
-                  const post = node.frontmatter
-                  if (
-                    post.published && post.tags.includes('dessert')
-                  ) {
-                    return (
-                      <li>
-                        <Link to={node.fields.slug}>
-                          <figure className={style.mediaContainer}>
-                            <img src={post.image1.childImageSharp.resolutions.src} srcSet={post.image1.childImageSharp.resolutions.srcSet} />
-                          </figure>
-                          <h3>{post.title}</h3>
-                        </Link>
-                      </li>
-                    )
-                  }
-                })}
-            </ul>
-          </li>
-          <li><Link to={'/'}>About</Link></li>
+          )
+        })}
         </ul>
       </header>
     )
---
+++ src/pages/2019-02-26-ground-turkey-shakshuka/index.md
@@ -0,0 +1,54 @@
+---
+title: "Ground Turkey Shakshuka"
+date: '2019-02-26'
+type: recipe
+funFact: "This was handcrafted with love by Ben, and I really adore when he makes it for me."
+image1: ./media/cover.jpg
+image2: ./media/secondary.jpg
+servings: 2
+netCarbs: 7.5
+calories: 328
+fat: 15
+protein: 38
+carbs: 13
+fiber: 6
+sugars: 0
+prepTime: "10 minutes"
+cookTime: "20 minutes"
+readyTime: "30 minutes"
+category: "breakfast"
+featured: true
+published: false
+tags:
+  - dinner
+  - lunch
+  - breakfast
+  - shakshuka
+  - tomato
+  - turkey
+---
+
+You'll be shook for this shakshuka. This meal is *so* good! Ahh! I absolutely love shakshuka, and when you make this variation with ground turkey it really takes it to the next level and turns it from a breakfast dish to an all-day dish. I've had this for breakfast, lunch, and dinner, and love it more and more each time.
+
+You can make this two ways. On the left is with __ tomatos and on the right is with whole tomatoes:
+
+![](/media/twoways.jpg)
+
+## You'll Need:
+
+- 8 oz  Frozen cooked shrimp (we used 100-200 size)
+- 12 oz zucchini noodles (or [spiraled zucchini](https://amzn.to/2PCaFzQ))
+- 4 tbsp (2 oz) [grass-fed butter](https://amzn.to/2MSRzHe)
+- 0.5 cup of riced cauliflower
+- 4 cloves of garlic, minced
+- 0.5 tbs garlic powder
+- Salt and pepper to taste
+- 0.5 cup celery, chopped
+
+## Directions:
+
+1. Sauté garlic in 2 tbsp of butter, then add celery and shrimp
+2. When shrimp is fully defrosted and simmering, add 2 more tbsp of butter with zucchini noodles. 
+3. Add garlic powder, salt, and pepper
+4. Continue to stir as you add the riced cauliflower. If desired, add more garlic powder
+5. Serve with a side salad (optional)
